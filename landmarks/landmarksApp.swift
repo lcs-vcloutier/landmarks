78e16cfb6266d682db065c057d71071841571d71
lcs-vcloutier <vcloutier@lcs.on.ca>
2021-12-09 10:35:19 -0500

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Vincent Cloutier on 2021-12-09.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
