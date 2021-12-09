bc59e7ac862c4b4043c4b5e4d38375de43105d30
lcs-vcloutier <vcloutier@lcs.on.ca>
2021-12-09 11:07:45 -0500

    landmark list view

---
+++ landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  landmarks
+//
+//  Created by Vincent Cloutier on 2021-12-09.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
