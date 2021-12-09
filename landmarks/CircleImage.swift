cfdddee4d456c936d8e653afcc802c05c040c609
lcs-vcloutier <vcloutier@lcs.on.ca>
2021-12-09 10:49:19 -0500

    completed image view

---
+++ landmarks/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  landmarks
+//
+//  Created by Vincent Cloutier on 2021-12-09.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+        
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
