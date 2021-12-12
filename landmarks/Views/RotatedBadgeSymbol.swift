7a567611878cf9bad9be5fa4904ed5889fc890c7
lcs-vcloutier <vcloutier@lcs.on.ca>
2021-12-12 18:25:12 -0500

    added badge symbol

---
+++ landmarks/Views/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  landmarks
+//
+//  Created by Vincent Cloutier on 2021-12-12.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
