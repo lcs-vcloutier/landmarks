69e2da9a07f6c4e8737f4d9f863533fa7ad5a9ef
lcs-vcloutier <vcloutier@lcs.on.ca>
2021-12-12 17:39:17 -0500

    added favourite button and added file structure

---
+++ landmarks/Views/Helpers/FavouriteView.swift
@@ -0,0 +1,28 @@
+//
+//  FavouriteView.swift
+//  landmarks
+//
+//  Created by Vincent Cloutier on 2021-12-12.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundColor(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+struct FavoriteButton_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteButton(isSet: .constant(true))
+    }
+}
