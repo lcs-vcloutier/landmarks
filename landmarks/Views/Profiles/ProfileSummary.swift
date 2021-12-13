cbcccfbc9b3e83a0fd630ee938268c41ab3ed91e
lcs-vcloutier <vcloutier@lcs.on.ca>
2021-12-12 19:31:51 -0500

    added hikebadge and updated profile summary with hike information

--- landmarks/Views/Profiles/ProfileSummary.swift
+++ landmarks/Views/Profiles/ProfileSummary.swift
@@ -10,17 +10,45 @@ import SwiftUI
 struct ProfileSummary: View {
     // The profile summary takes a Profile value rather than a binding to the profile because the parent view, ProfileHost, manages the state for this view.
     var profile: Profile
-
+    @EnvironmentObject var modelData: ModelData
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading, spacing: 10) {
                 Text(profile.username)
                     .bold()
                     .font(.title)
-
+                
                 Text("Notifications: \(profile.prefersNotifications ? "On": "Off" )")
                 Text("Seasonal Photos: \(profile.seasonalPhoto.rawValue)")
                 Text("Goal Date: ") + Text(profile.goalDate, style: .date)
+                
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Completed Badges")
+                        .font(.headline)
+                    
+                    ScrollView(.horizontal) {
+                        HStack {
+                            HikeBadge(name: "First Hike")
+                            HikeBadge(name: "Earth Day")
+                                .hueRotation(Angle(degrees: 90))
+                            HikeBadge(name: "Tenth Hike")
+                                .grayscale(0.5)
+                                .hueRotation(Angle(degrees: 45))
+                        }
+                        .padding(.bottom)
+                    }
+                }
+                Divider()
+                
+                VStack(alignment: .leading) {
+                    Text("Recent Hikes")
+                        .font(.headline)
+                    
+                    HikeView(hike: modelData.hikes[0])
+                }
             }
         }
     }
