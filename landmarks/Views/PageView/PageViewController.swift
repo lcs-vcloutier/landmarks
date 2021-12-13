091cb75e863a1bba42b29c89e519b814576d118e
lcs-vcloutier <vcloutier@lcs.on.ca>
2021-12-12 20:15:14 -0500

    enable swiping interactions to move from page to page

--- landmarks/Views/PageView/PageViewController.swift
+++ landmarks/Views/PageView/PageViewController.swift
@@ -12,19 +12,65 @@ import UIKit
 // The page view controller stores an array of Page instances, which must be a type of View. These are the pages you use to scroll between landmarks.
 struct PageViewController<Page: View>: UIViewControllerRepresentable {
     var pages: [Page]
-
-// SwiftUI calls this method a single time when it’s ready to display the view, and then manages the view controller’s life cycle.
+    
+    // SwiftUI calls this makeCoordinator() method before makeUIViewController(context:), so that you have access to the coordinator object when configuring your view controller.
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    // SwiftUI calls this method a single time when it’s ready to display the view, and then manages the view controller’s life cycle.
     func makeUIViewController(context: Context) -> UIPageViewController {
         let pageViewController = UIPageViewController(
             transitionStyle: .scroll,
             navigationOrientation: .horizontal)
-
+        pageViewController.dataSource = context.coordinator
+        
         return pageViewController
     }
-
-// create the UIHostingController that hosts the page SwiftUI view on every update.
+    
+    // create the UIHostingController that hosts the page SwiftUI view on every update.
     func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
         pageViewController.setViewControllers(
-            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+            [context.coordinator.controllers[0]], direction: .forward, animated: true)
     }
+    
+    // A SwiftUI view that represents a UIKit view controller can define a Coordinator type that SwiftUI manages and provides as part of the representable view’s context.
+    //SwiftUI manages the UIViewControllerRepresentable type’s coordinator, and provides it as part of the context when calling the methods you created above.
+    class Coordinator: NSObject, UIPageViewControllerDataSource {
+        var parent: PageViewController
+        var controllers = [UIViewController]()
+        
+        init(_ pageViewController: PageViewController) {
+            parent = pageViewController
+            controllers = parent.pages.map { UIHostingController(rootView: $0) }
+        }
+        
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerBefore viewController: UIViewController) -> UIViewController?
+        {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index == 0 {
+                return controllers.last
+            }
+            return controllers[index - 1]
+        }
+        
+        func pageViewController(
+            _ pageViewController: UIPageViewController,
+            viewControllerAfter viewController: UIViewController) -> UIViewController?
+        {
+            guard let index = controllers.firstIndex(of: viewController) else {
+                return nil
+            }
+            if index + 1 == controllers.count {
+                return controllers.first
+            }
+            return controllers[index + 1]
+        }
+    }
+    
+    
 }
