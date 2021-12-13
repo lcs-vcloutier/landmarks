e8eef490ad2988b4421a16edabaf1df1d17c035e
lcs-vcloutier <vcloutier@lcs.on.ca>
2021-12-12 20:46:56 -0500

    added notification

--- landmarks/landmarksApp.swift
+++ landmarks/landmarksApp.swift
@@ -10,10 +10,13 @@ import SwiftUI
 @main
 struct landmarksApp: App {
     @StateObject private var modelData = ModelData()
-
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+#if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+#endif
     }
 }
