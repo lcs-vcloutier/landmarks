7d629ebdd35f73da662a678074ac1b6f73dd9139
lcs-vcloutier <vcloutier@lcs.on.ca>
2021-12-12 20:09:19 -0500

    added swiftui view for pageview

---
+++ landmarks/Views/PageView/PageView.swift
@@ -0,0 +1,23 @@
+//
+//  PageView.swift
+//  landmarks
+//
+//  Created by Vincent Cloutier on 2021-12-12.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+
+    var body: some View {
+        PageViewController(pages: pages)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+            .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
