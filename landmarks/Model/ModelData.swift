1061f7b3f9e40bd4b5175848e728c53f9f58e4c7
lcs-vcloutier <vcloutier@lcs.on.ca>
2021-12-12 19:09:31 -0500

    category list

--- landmarks/Model/ModelData.swift
+++ landmarks/Model/ModelData.swift
@@ -12,6 +12,13 @@ final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
     // Hikes are not @Published because you want have to modify it
     var hikes: [Hike] = load("hikeData.json")
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
 }
 
 var landmarks: [Landmark] = load("landmarkData.json")
