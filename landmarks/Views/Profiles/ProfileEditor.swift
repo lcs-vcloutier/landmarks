be5e907c45083809fb72fdd4d459ec3a537082ea
lcs-vcloutier <vcloutier@lcs.on.ca>
2021-12-12 19:48:28 -0500

    added basic profile editor view

---
+++ landmarks/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileEditor.swift
+//  landmarks
+//
+//  Created by Vincent Cloutier on 2021-12-12.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    @Binding var profile: Profile
+    
+    var body: some View {
+        List {
+            HStack {
+                Text("Username").bold()
+                Divider()
+                TextField("Username", text: $profile.username)
+            }
+        }
+    }
+}
+
+struct ProfileEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditor(profile: .constant(.default))
+    }
+}
