cbccbee5f2ee4f1be1c19dc3fb99e3834998d20f
lcs-vcloutier <vcloutier@lcs.on.ca>
2021-12-12 19:25:57 -0500

    created profile data

---
+++ landmarks/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  landmarks
+//
+//  Created by Vincent Cloutier on 2021-12-12.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+    static let `default` = Profile(username: "vcloutier")
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { rawValue }
+    }
+}
